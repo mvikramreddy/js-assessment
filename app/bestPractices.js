697ef20a5fb10339f16ee36ee1412214455e20bc
mvikramreddy <vikramreddym@hotmail.com>
2015-08-18 15:33:38 -0700

    bestPratices.cs changes

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -5,32 +5,68 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
  * populated incorrectly; your job is to fix them. Other methods are not
  * populated at all; your job is to fill them out.
  */
-define(function() {
-  return {
-    globals : function() {
-      myObject = {
-        name : 'Jory'
-      };
+define(function () {
+    return {
+        globals: function () {
+            myObject = {
+                name: 'Jory'
+            };
 
-      return myObject;
-    },
+            return myObject;
+        },
 
-    functions : function(flag) {
-      if (flag) {
-        function getValue() { return 'a'; }
-      } else {
-        function getValue() { return 'b'; }
-      }
+        functions: function (flag) {
 
-      return getValue();
-    },
+            function getValue() {
+                if (flag) {
+                    return 'a';
+                }
+                else {
+                    return 'b';
+                }
+            }
 
-    parseInt : function(num) {
-      return parseInt(num);
-    },
+            return getValue;
+        },
 
-    identity : function(val1, val2) {
+        parseInt: function (num) {
+            return Integer.parseInt(num);
+        },
 
-    }
-  };
+        identity: function (val1, val2) {
+            if (val1 === val2) {
+                console.log(val1 + ' is the same as ' + val2);
+                return true;
+            } else if (typeof val1 === 'object' && typeof val2 === 'object'
+                    && val1 !== null && val2 !== null) {
+                var val1Keys = Object.keys(val1);
+                var val2Keys = Object.keys(val2);
+
+                var arr1Length = val1Keys.length;
+                if (arr1Length === val2Keys.length) {
+                    for (var i = 0; i < arr1Length; i++) {
+                        console.log('Looking at key ' + val1Keys[i]);
+                        if (val1Keys[i] === val2Keys[i]) {
+                            console.log('Checking whether ' + val1[val1Keys[i]] +
+                                ' is the same as ' + val2[val2Keys[i]]);
+                            if (!deepEqual(val1[val1Keys[i]], val2[val2Keys[i]])) {
+                                return false;
+                            }
+                        } else {
+                            // not the same value for the current key
+                            return false;
+                        }
+                    }
+                } else {
+                    // not the same length of keys
+                    return false;
+                }
+
+                return true;
+            } else {
+                // they're either not objects or null
+                return false;
+            }
+        }
+    };
 });
