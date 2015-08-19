acc004fd64fbada49460e1c15bde4e2adb7fef65
mvikramreddy <vikramreddym@hotmail.com>
2015-08-19 09:09:33 -0700

    ASYNC.JS CHANGES

--- app/async.js
+++ app/async.js
@@ -1,13 +1,27 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define([ 'jquery' ], function($) {
-  return {
-    async : function(value) {
-         test
-    },
+define(['jquery'], function ($) {
+    return {
+         then: function(callback){
+          callback(true);
+        }
+        },
 
-    manipulateRemoteData : function(url) {
+        manipulateRemoteData: function (url) {
+           
+            var request = new XMLHttpRequest();
+              return {
+                then: function(callback) {
+                  request.onreadystatechange = function(){
+                    if (request.readyState == 4 && request.status >= 200 && request.status < 400) {
+                      var result = JSON.parse(request.responseText);
+                      return callback(result);
+                    }
+                  }
+                  request.open("GET", url, true);
+                  request.send();
 
-    }
-  };
+
+        }
+    };
 });
