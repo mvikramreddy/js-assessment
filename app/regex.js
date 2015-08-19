f0e2e18781d1d3f188045f3cdc5f3a0f9cfaf88d
mvikramreddy <vikramreddym@hotmail.com>
2015-08-18 17:03:29 -0700

    regex changes

--- app/regex.js
+++ app/regex.js
@@ -1,28 +1,33 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define(function() {
-  return {
-    containsNumber : function(str) {
+define(function () {
+    return {
+        containsNumber: function (str) {
+            var pattern = "/^\d+$/";
+            return pattern.test(str);
+        },
 
-    },
+        containsRepeatingLetter: function (str) {
+            var pattern = "/([a-zA-Z]).*?\1/";
+            return pattern.test(str);
+        },
 
-    containsRepeatingLetter : function(str) {
+        endsWithVowel: function (str) {
+            var pattern = "/[aeiou]/";
+            return pattern.test(str[str.length - 1]);
+        },
 
-    },
+        captureThreeNumbers: function (str) {
 
-    endsWithVowel : function(str) {
+        },
 
-    },
-
-    captureThreeNumbers : function(str) {
-
-    },
-
-    matchesPattern : function(str) {
-
-    },
-    isUSD : function(str) {
-
-    }
-  };
+        matchesPattern: function (str) {
+            var pattern = "????";
+            return pattern.test(str);
+        },
+        isUSD: function (str) {
+           var pattern = "^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?$";
+            return pattern.test(str); 
+        }
+    };
 });
