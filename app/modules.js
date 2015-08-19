04cec9377e4b8ed1e51cc9eca92d90ed1c12824b
mvikramreddy <vikramreddym@hotmail.com>
2015-08-19 09:22:41 -0700

    module changes

--- app/modules.js
+++ app/modules.js
@@ -3,7 +3,13 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
-
+        return {
+        name: str1,
+        department: str2,
+        printme : function(){
+          return "" + this.name + ", test";
+        }
+      }
     }
   };
 });
