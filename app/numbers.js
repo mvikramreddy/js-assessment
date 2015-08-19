bdf43a8b47041d2497a93e30ccd51f9264856de1
mvikramreddy <vikramreddym@hotmail.com>
2015-08-19 09:43:09 -0700

    numbers and objects changes

--- app/numbers.js
+++ app/numbers.js
@@ -1,22 +1,29 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define(function() {
-  return {
-    valueAtBit: function(num, bit) {
+define(function () {
+    return {
+        valueAtBit: function (num, bit) {
+            // i could do parseint(num, 2) to get the bits then return just the index
 
-    },
+            return parseInt(num, 2)[bit];
+        },
 
-    base10: function(str) {
+        base10: function (str) {
+            return parseInt(str); //default base 10
+        },
 
-    },
+        convertToBinary: function (num) {
+            if (dec >= 0) {
+                return dec.toString(2);
+            }
+            else {
+                return (~dec).toString(2);
+            }
+        },
 
-    convertToBinary: function(num) {
-
-    },
-
-    multiply: function(a, b) {
-
-    }
-  };
+        multiply: function (a, b) {
+            return a * b;
+        }
+    };
 });
 
