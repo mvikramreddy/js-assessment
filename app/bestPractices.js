ef71909fae743e2d7f95848e3f4ad693b5946bb0
mvikramreddy <vikramreddym@hotmail.com>
2015-08-19 10:47:10 -0700

    errors

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -17,7 +17,7 @@ define(function () {
 
         functions: function (flag) {
 
-           var getValue =  function (flag) {
+            var getValue = function (flag) {
                 if (flag) {
                     return 'a';
                 }
@@ -34,39 +34,7 @@ define(function () {
         },
 
         identity: function (val1, val2) {
-            if (val1 === val2) {
-                console.log(val1 + ' is the same as ' + val2);
-                return true;
-            } else if (typeof val1 === 'object' && typeof val2 === 'object'
-                    && val1 !== null && val2 !== null) {
-                var val1Keys = Object.keys(val1);
-                var val2Keys = Object.keys(val2);
-
-                var arr1Length = val1Keys.length;
-                if (arr1Length === val2Keys.length) {
-                    for (var i = 0; i < arr1Length; i++) {
-                        console.log('Looking at key ' + val1Keys[i]);
-                        if (val1Keys[i] === val2Keys[i]) {
-                            console.log('Checking whether ' + val1[val1Keys[i]] +
-                                ' is the same as ' + val2[val2Keys[i]]);
-                            if (!deepEqual(val1[val1Keys[i]], val2[val2Keys[i]])) {
-                                return false;
-                            }
-                        } else {
-                            // not the same value for the current key
-                            return false;
-                        }
-                    }
-                } else {
-                    // not the same length of keys
-                    return false;
-                }
-
-                return true;
-            } else {
-                // they're either not objects or null
-                return false;
-            }
+            return (val1 === val2);
         }
     };
 });
